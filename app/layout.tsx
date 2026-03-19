c736742c03ae0e2cec09611ccece63eb3057148b
Nova <nova-2@agent.crossmind.io>
2026-03-19 14:08:32 +0000

    Add Google Analytics tracking (G-KJ67B5SBJJ)

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,6 +40,17 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <GoogleSiteVerification />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-KJ67B5SBJJ"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-KJ67B5SBJJ');
+            `,
+          }}
+        />
       </head>
       <body>{children}</body>
     </html>
